336a66ec61ebd389060d9ff80378eb66e017c9bf
Tevin-creator <tevin.isaac@student.moringaschool.com>
2021-06-14 15:48:13 +0300

    made changes in index.html and styles.css

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function submit() {
         output.innerHTML = "Hello" + " " + name + ",   " + "you were born on a" + " " + daysOfTheWeek[answer] + "." + " " + "Suprise!!, your Akan name is:" + femaleNames[answer]
 
     } else if (male.checked && year > 0 && year < 2020 && month > 0 && month < 12 && day > 0 && day < 31) {
-        output.style.background = "blue"
+        output.style.background = "green"
         output.innerHTML = "Hello" + " " + name + ",   " + "you were born on a" + " " + daysOfTheWeek[answer] + "." + " " + "Suprise!!, your Akan name is:" + maleNames[answer]
     } else if (female.checked && year > 0 && year < 2020 && month > 0 && month < 12 && day > 0 && day < 31) {
         output.style.background = "yellow"
@@ -49,90 +49,19 @@ document.querySelector("#details").addEventListener("submit", function(event) {
 
 })
 
-
-(function($) {
-
-    // Remove no-js class
-    $('html').removeClass('no-js');
-
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
-
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
-
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
-
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
-
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
         }
     });
-
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
+    // scroll body to 0px on click
+    $('#back-to-top').click(function() {
+        $('body,html').animate({
             scrollTop: 0
-        }, 500);
+        }, 400);
+        return false;
     });
-
-
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
-
-    // Create timeline
-    $('#experience-timeline').each(function() {
-
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
-
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
-
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
-
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
-            }
-        });
-
-    });
-
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
-
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
-
-    // Load additional projects
-    $('#view-more-projects').click(function(e) {
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
-
-})(jQuery);
+});
